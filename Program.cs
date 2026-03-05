ce5bedaec528b1bc605b49223006c1470250a742
Jan Jones <janjones@microsoft.com>
2026-03-05 15:04:23 -0800

    Remove workaround targets

--- Program.cs
+++ Program.cs
@@ -26,51 +26,6 @@ void BuildInMemoryProject()
 
             <Import Project="Sdk.targets" Sdk="Microsoft.NET.Sdk" />
 
-            <!-- Override targets which don't work with project files that are not present on disk. -->
-
-            <Target Name="_FilterRestoreGraphProjectInputItems"
-                    DependsOnTargets="_LoadRestoreGraphEntryPoints">
-                <!-- No-op, the original output is not needed by the overwritten targets. -->
-            </Target>
-
-            <Target Name="_GetAllRestoreProjectPathItems"
-                    DependsOnTargets="_FilterRestoreGraphProjectInputItems;_GenerateRestoreProjectPathWalk"
-                    Returns="@(_RestoreProjectPathItems)">
-                <!-- Output from dependency _GenerateRestoreProjectPathWalk. -->
-            </Target>
-
-            <Target Name="_GenerateRestoreGraph"
-                    DependsOnTargets="_FilterRestoreGraphProjectInputItems;_GetAllRestoreProjectPathItems;_GenerateRestoreGraphProjectEntry;_GenerateProjectRestoreGraph"
-                    Returns="@(_RestoreGraphEntry)">
-                <!-- Output partly from dependency _GenerateRestoreGraphProjectEntry and _GenerateProjectRestoreGraph. -->
-
-                <ItemGroup>
-                    <_GenerateRestoreGraphProjectEntryInput Include="@(_RestoreProjectPathItems)" Exclude="$(MSBuildProjectFullPath)" />
-                </ItemGroup>
-
-                <MSBuild
-                    BuildInParallel="$(RestoreBuildInParallel)"
-                    Projects="@(_GenerateRestoreGraphProjectEntryInput)"
-                    Targets="_GenerateRestoreGraphProjectEntry"
-                    Properties="$(_GenerateRestoreGraphProjectEntryInputProperties)">
-
-                <Output
-                    TaskParameter="TargetOutputs"
-                    ItemName="_RestoreGraphEntry" />
-                </MSBuild>
-
-                <MSBuild
-                    BuildInParallel="$(RestoreBuildInParallel)"
-                    Projects="@(_GenerateRestoreGraphProjectEntryInput)"
-                    Targets="_GenerateProjectRestoreGraph"
-                    Properties="$(_GenerateRestoreGraphProjectEntryInputProperties)">
-
-                <Output
-                    TaskParameter="TargetOutputs"
-                    ItemName="_RestoreGraphEntry" />
-                </MSBuild>
-            </Target>
-
         </Project>
         """;
 
@@ -81,7 +36,11 @@ void BuildInMemoryProject()
     };
 
     var projectCollection = new ProjectCollection(
-        globalProperties: new Dictionary<string, string>(),
+        globalProperties: new Dictionary<string, string>
+        {
+            { "_BuildNonexistentProjectsByDefault", bool.TrueString },
+            { "RestoreUseSkipNonexistentTargets", bool.FalseString },
+        },
         loggers: loggers,
         ToolsetDefinitionLocations.Default
     );
